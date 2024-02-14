34c05efa88e8d8a3bce25098595bdb94ec6b7f5f
Hashir <Hashiraslam2015@gmail.com>
2024-02-14 11:17:14 +0530

    Complete why-yumyum page of yumyumcloud.com

--- Project/App/urls.py
+++ Project/App/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about-yumyum/', views.about, name='about-yumyum'),
+    path('why-yumyum', views.why_yumyum, name='why-yumyum'),
+
 ]
