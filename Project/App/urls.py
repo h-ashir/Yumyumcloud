0e88fec907428c81cb323a259052271f342c6a76
Hashir <Hashiraslam2015@gmail.com>
2024-02-16 10:04:33 +0530

    Half of the Menu section completed

--- Project/App/urls.py
+++ Project/App/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('about-yumyum/', views.about, name='about-yumyum'),
     path('why-yumyum/', views.why_yumyum, name='why-yumyum'),
     path('contact-us/', views.contact, name='contact-us'),
-    # path('menu/', views.menu, name='menu'),
+    path('menu/', views.menu, name='menu'),
     # path('photo/', views.photo, name='photo'),
     # path('contact-us/', views.contact, name='contact-us'),
 
